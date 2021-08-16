09d462639522d74e94c1ff4af9e9f98424ff3b30
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-16 15:42:22 +0100

    Part 4

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,9 +1,14 @@
 function printNumbers(){
     for (i = 1; i <= 100; i++){
-        if (i % 3 == 0){
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz")
+        } else if (i % 3 == 0){
             console.log("Fizz")
-            continue;
-        } else console.log(i)
+        } else if (i % 5 == 0){
+            console.log("Buzz")
+        } else console.log(i);
+            
+            
     }
 }
 printNumbers();
