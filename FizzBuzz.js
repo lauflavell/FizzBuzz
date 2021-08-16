f777eddbaa2099c3f2c6d7c70ee064f2524ff705
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-16 17:45:03 +0100

    Refactored part 4

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,14 +1,16 @@
+function isDivisable(n, m){
+    return n % m == 0;
+}
+
 function printNumbers(){
     for (i = 1; i <= 100; i++){
-        if (i % 3 == 0 && i % 5 == 0){
+        if (isDivisable(i, 3) && isDivisable(i, 5)){
             console.log("FizzBuzz")
-        } else if (i % 3 == 0){
+        } else if (isDivisable(i, 3)){
             console.log("Fizz")
-        } else if (i % 5 == 0){
+        } else if (isDivisable(i, 5)){
             console.log("Buzz")
-        } else console.log(i);
-            
-            
+        } else console.log(i);      
     }
 }
 printNumbers();
