3046b9a251126595d2e86e5f78d2900abdbf8b69
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-16 15:24:27 +0100

    Part 3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,8 +1,9 @@
-console.log("Hello World");
-
 function printNumbers(){
     for (i = 1; i <= 100; i++){
-        console.log(i)
+        if (i % 3 == 0){
+            console.log("Fizz")
+            continue;
+        } else console.log(i)
     }
 }
 printNumbers();
