e4f9c441257015e44b8b36bb82b96ffdd4573400
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-30 17:11:56 +0100

    print Bang, Bong, Fezz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,14 +3,36 @@ function isDivisable(n, m){
 }
 
 function printNumbers(){
-    for (i = 1; i <= 100; i++){
-        if (isDivisable(i, 3) && isDivisable(i, 5)){
-            console.log("FizzBuzz")
-        } else if (isDivisable(i, 3)){
-            console.log("Fizz")
-        } else if (isDivisable(i, 5)){
-            console.log("Buzz")
-        } else console.log(i);      
+    for (i = 1; i <= 200; i++){
+        let print = "";
+        if (isDivisable(i, 3)){
+            print += "Fizz"
+        }
+        if (isDivisable(i, 5)){
+            print += "Buzz"
+        } 
+        if (isDivisable(i, 7)){
+            print += "Bang"
+        }
+        if (isDivisable(i, 11)){
+            print = "Bong"
+        }
+        if (isDivisable(i, 13)){
+            if(print.startsWith('B')){
+                print = 'Fezz' + print.substring(0)
+            }
+            else if(print.startsWith('F')){
+                let printArr = print.split('')
+                printArr.splice(4,0,'Fezz')
+                print = printArr.join('');
+            }
+            else print = 'Fezz'
+        }
+        else if (print === "")   {
+            print = i;
+        }  
+        console.log(print) 
     }
+    
 }
 printNumbers();
